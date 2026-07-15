ed6b71ce7e8871d8211107785e4b284b64d5bd6b
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-15 19:09:49 +0900

    [feat]Set screen orientation based on device

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,36 @@ import 'package:finbrain/themes/color_theme.dart';
 import 'package:finbrain/ui/screen/onboarding_screen.dart';
 import 'package:finbrain/ui/viewModel/text_theme_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding binding =  WidgetsFlutterBinding.ensureInitialized();
+
+  final view = binding.platformDispatcher.views.first;
+  final size = view.physicalSize / view.devicePixelRatio;
+  final isPhone = size.shortestSide < 600;
+  if(isPhone){
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+  } else {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+
   await dotenv.load(fileName: "assets/keys.env");
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
   runApp(const ProviderScope(child: FinBrain()));
 }
 
