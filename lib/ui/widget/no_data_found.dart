b8955e25c82a6232769c25e34e287ac728ca2ff8
Daeun Lee <dev.leedaeun@gmail.com>
2026-07-17 19:34:45 +0900

    [style] Changed design of the search result limitation notice

--- lib/ui/widget/no_data_found.dart
+++ lib/ui/widget/no_data_found.dart
@@ -9,18 +9,22 @@ class NoDataFound extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
+    final style = textTheme.bodyMedium!.copyWith(
+      color: colorScheme.onSecondary,
+    );
 
     return Center(
-      child: Text(
-        (isProduct) ?
-        """
-        검색 조건에 맞는 상품이 없습니다
-
-        아직 불러오지 않은 상품이 있을 수 있으니,
-        화면을 아래로 스크롤하여 더 많은 상품을 확인한 뒤 다시 시도해주세요
-        """ : "검색 조건과 일치하는 데이터가 없습니다.",
-        style: textTheme.bodyMedium!.copyWith(color: colorScheme.onSecondary),
-      ),
+      child: (isProduct)
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("검색 조건에 맞는 상품이 없습니다", style: style),
+                const SizedBox(height: 16.0),
+                Text("아직 불러오지 않은 상품이 있을 수 있으니, 화면을 아래로 스크롤하여 더 많은 상품을 확인 후  다시 시도해주세요", style: style),
+              ],
+            )
+          : Text("검색 조건과 일치하는 데이터가 없습니다.", style: style),
     );
   }
 }
